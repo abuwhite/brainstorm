c52efd883a9a7ea9c1ef8e9954cd429ddd6aae29
notabu <samyyklass@gmail.com>
2021-04-22 19:01:51 +0600

    fix problems

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -6,13 +6,13 @@ from brain_games import cli
 
 
 def greeting():
-    """Make a user interface."""
+    """General user greeting."""
     print('Welcome to the Brain Games!')
 
 
 def user_greeting(user_name):
-    """Make a user interface."""
-    return print('Hello, {user}!'.format(user=user_name))
+    """Player's greeting."""
+    return 'Hello, {user}!'.format(user=user_name)
 
 
 def main():
