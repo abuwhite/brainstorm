d6f608301e7259e8366a90494f1babc81a5be119
notabu <samyyklass@gmail.com>
2021-04-25 10:37:47 +0600

    add script game

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,47 +1,11 @@
-"""Main cli module for even game."""
+"""The main client module of the game even."""
 
-import prompt
-import random
-from brain_games.scripts.brain_games import greeting, user_greeting
-from brain_games.cli import get_name
-
-
-def is_even(num):
-    if (num % 2) == 0:
-        return 'yes'
-    return 'no'
-
-
-def even(name):
-    counter = 0
-    round_count = 3
-    while counter < round_count:
-        question = random.randint(1, 10)
-        print('Question: {random}'.format(random=question))
-
-        user_answer = prompt.string('Your answer: ')
-        correct_answer = is_even(question)
-
-        if user_answer != correct_answer:
-            return print('\'{wrong}\' is wrong answer ;(. Correct answer was \'{correct}\'. '
-                         '\nLet\'s try again, {user}!'.format(wrong=user_answer,
-                                                              correct=correct_answer,
-                                                              user=name))
-
-        print('Correct!')
-        counter += 1
-
-    print('Congratulations, {user}!'.format(user=name))
+from brain_games.scripts.game_engine import game_engine
+from brain_games.games.even import generate_round
 
 
 def main():
-    greeting()
-    user_name = get_name()
-
-    print(user_greeting(user_name))
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-
-    even(user_name)
+    game_engine(generate_round)
 
 
 if __name__ == '__main__':
