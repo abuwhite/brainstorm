268bd752bfcdde99a582da32a68419cc8231da2f
notabu <samyyklass@gmail.com>
2021-04-27 13:49:03 +0600

    fix

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,11 @@
 """The main client module of the game even."""
 
 from brain_games.scripts.game_engine import game_engine
-from brain_games.games.even import generate_round
+from brain_games.games.even import generate_round, RULES
 
 
 def main():
-    game_engine(generate_round)
+    game_engine(generate_round, RULES)
 
 
 if __name__ == '__main__':
