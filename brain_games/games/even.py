447c193cb48fd40d914def6b299a3844261eb154
notabu <samyyklass@gmail.com>
2021-04-27 15:41:54 +0600

    fix problems

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,21 +2,25 @@
 
 import random
 
-# Правила игры
-RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
-
 
 def is_even(num):
-    if (num % 2) == 0:
-        return 'yes'
-    return 'no'
+    return True if (num % 2) == 0 else False
+
+
+def get_answer(question):
+    return 'yes' if is_even(question) else 'no'
 
 
 def generate_number():
     return random.randint(1, 10)
 
 
+# Правила игры
+RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+# Генерируем вопрос и правильный ответ
 def generate_round():
     question = generate_number()
-    answer = is_even(question)
+    answer = get_answer(question)
     return question, answer
