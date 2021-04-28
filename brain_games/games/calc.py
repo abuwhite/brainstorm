042427eb7310c131ecc0ec58f17ffea1cf81211b
notabu <samyyklass@gmail.com>
2021-04-28 12:30:23 +0600

    add logic

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,35 +3,29 @@
 import random
 
 
-def generate_correct_answer(first, second, operator):
+def calculate_expression(first_num, second_num, operator):
     if operator == '+':
-        return str(first + second)
+        return str(first_num + second_num)
     if operator == '-':
-        return str(first - second)
+        return str(abs(first_num - second_num))
     if operator == '*':
-        return str(first * second)
-
-
-def generate_operator():
-    operators = '+-*'
-    return random.choice(operators)
-
-
-def generate_number():
-    return random.randint(1, 10)
+        return str(first_num * second_num)
 
 
 def generate_question():
-    first_num = generate_number()
-    second_num = generate_number()
-    operator = generate_operator()
-    return first_num, second_num, operator
+    first_num = random.randint(1, 10)
+    second_num = random.randint(1, 10)
+    operator = random.choice('+-*')
+    return first_num, operator, second_num
 
 
-def generate_round(condition=False):
-    if condition == 'condition':
-        return 'What is the result of the expression?'
-    (first, second, op) = generate_question()
-    question = '{one} {op} {two}'.format(one=first, two=second, op=op)
-    answer = generate_correct_answer(first, second, op)
+# Правила игры
+RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+# Генерируем вопрос и правильный ответ
+def generate_round():
+    (num_1, op, num_2) = generate_question()
+    question = '{num_1} {op} {num_2}'.format(num_1=num_1, num_2=num_2, op=op)
+    answer = calculate_expression(num_1, num_2, op)
     return question, answer
