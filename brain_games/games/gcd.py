4cdaa193b8b228dd7c77d49727a716c684aacc1e
notabu <samyyklass@gmail.com>
2021-04-29 15:54:48 +0600

    add logic gcd game

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,8 +1,23 @@
 """Module with the logic of the game gcd."""
+import math
+import random
+
+
+def generate_question():
+    """Генерируем первое и второе число от 1 до 100.
+
+    Returns:
+        int: Возращаем первое случайно сгенерированное число.
+        int: Возращаем второе случайно сгенерированное число.
+
+    """
+    first_num = random.randint(1, 100)
+    second_num = random.randint(1, 100)
+    return first_num, second_num
 
 
 # Правила игры
-RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
+RULES = 'Find the greatest common divisor of given numbers.'
 
 
 def generate_round():
@@ -13,6 +28,7 @@ def generate_round():
         question: Вопрос пользователю
         result: Правильный ответ на вопрос
     """
-    question = generate_number()
-    answer = get_correct_result(question)
+    first_num, second_num = generate_question()
+    question = '{one} {two}'.format(one=first_num, two=second_num)
+    answer = str(math.gcd(first_num, second_num))
     return question, answer
