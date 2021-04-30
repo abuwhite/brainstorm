d6a6cf0678f860cb3b5564f4864ea72dd3eaf2fa
notabu <samyyklass@gmail.com>
2021-04-30 15:18:42 +0600

    fix utf-8

--- brain_games/games/__init__.py
+++ brain_games/games/__init__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """This is an example script."""
 from brain_games.games import calc, even, gcd, prime, progression  # noqa: F401
 
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """The main client module of the game gcd."""
 
