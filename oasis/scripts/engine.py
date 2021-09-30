634382c53c25523ea932ac893b3cad68af76c68e
Борис Женихов <znhv@yandex.com>
2021-09-30 18:15:22 +0600

    Add player ready function

--- oasis/scripts/engine.py
+++ oasis/scripts/engine.py
@@ -7,6 +7,11 @@ from oasis.config.config import NUMBER_ROUNDS
 from termcolor import colored, cprint
 
 
+def player_ready():
+    answer = prompt.string("Are you ready? (y/n): ")
+    return True if answer == 'y' else False
+
+
 def run(game, player_name):
     """Start engine game.
 
@@ -19,33 +24,35 @@ def run(game, player_name):
     """
     print(game.RULES, "\n")
 
-    score = 0
-    round_count = 0  # Rounds counter
-    while round_count < NUMBER_ROUNDS:
-        question, correct = game.generate_round()
+    ready = player_ready()
+    if ready:
+        score = 0
+        round_count = 0  # Rounds counter
+        while round_count < NUMBER_ROUNDS:
+            question, correct = game.generate_round()
 
-        # Ask player
-        print("Question: {random}".format(random=question))
+            # Ask player
+            print("Question: {random}".format(random=question))
 
-        # Receive player answer
-        answer = prompt.string("Your answer: ")
+            # Receive player answer
+            answer = prompt.string("Your answer: ")
 
-        if answer != correct:
-            first = "'{a}' is wrong answer ;(.".format(a=answer)
-            second = "Correct answer was '{b}'".format(b=correct)
-            print(colored(first, "red"), colored(second, "green"))
-            print("Let's try again, {user}!\n".format(user=player_name))
-            break
+            if answer != correct:
+                first = "'{a}' is wrong answer ;(.".format(a=answer)
+                second = "Correct answer was '{b}'".format(b=correct)
+                print(colored(first, "red"), colored(second, "green"))
+                print("Let's try again, {user}!\n".format(user=player_name))
+                break
 
-        cprint("Correct!", "green")
-        round_count += 1
-        score += 1
+            cprint("Correct!", "green")
+            round_count += 1
+            score += 1
 
-    else:
-        cprint(
-            "Congratulations, {user}!\n".format(user=player_name),
-            "magenta",
-            attrs=["bold"],
-        )
+        else:
+            cprint(
+                "Congratulations, {user}!\n".format(user=player_name),
+                "magenta",
+                attrs=["bold"],
+            )
 
-    return score
+        return score
