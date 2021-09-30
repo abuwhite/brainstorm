722e8d32b211415c2ee68bc12777dd965f28034b
Борис Женихов <znhv@yandex.com>
2021-09-30 18:15:45 +0600

    Add test for player_ready function

---
+++ tests/test_engine.py
@@ -0,0 +1,13 @@
+import unittest.mock as mock
+import pytest
+
+from oasis.scripts import engine
+from oasis.games import calc
+
+
+def test_player_ready(monkeypatch):
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda prompt="": "y")
+        result = engine.player_ready()
+    assert result is True
+
